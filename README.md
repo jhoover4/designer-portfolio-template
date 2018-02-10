398bdf066ed479db7dbb6a7a821a6b533b9a761a
jhoover4 <jordan@hoovermld.com>
2018-02-10 17:07:40 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal Portfolio
+
+My source code for my portfolio page. Hosting to come soon.
---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+DEBUG = True
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/work')
+def portfolio():
+	return render_template('portfolio.html')
+
+if __name__ == '__main__':
+	app.run(debug=DEBUG)
