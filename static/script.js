256af367a3ca2b38755b1199f895701e7ff11dcd
Jordan Hoover <jordan@hoovermld.com>
2018-02-12 17:03:59 -0600

    feature/basic footer animations

--- static/script.js
+++ static/script.js
@@ -1,1 +1,22 @@
-console.log("Javascript working");
+
+$( document ).ready(function() {
+  console.log("Javascript working");
+  // scroll reveal
+  window.sr = ScrollReveal({ reset: true });
+  sr.reveal('.email-sign-up .before-input', { duration: 300, easing: 'ease-out'});
+  sr.reveal('.email-sign-up input', { duration: 1000, beforeReveal: function (domEl) {
+    $(domEl).next().addClass('border-bar-activate');
+  },
+  afterReset: function (domEl) {
+    $(domEl).next().removeClass('border-bar-activate');
+  }
+});
+
+  // hit enter fadein
+  $input = $(".email-sign-up input");
+  $input.hover(function(){
+      $(".after-input").fadeIn();
+    }, function() {
+      $(".after-input").fadeOut();
+  });
+});
