4dacb7803108b31b5d2ba83aef3419fd5923467f
Jordan Hoover <jordan@hoovermld.com>
2018-02-13 16:42:28 -0600

    Feature/Add text scroll

--- static/script.js
+++ static/script.js
@@ -15,8 +15,59 @@ $( document ).ready(function() {
   // hit enter fadein
   $input = $(".email-sign-up input");
   $input.hover(function(){
-      $(".after-input").fadeIn();
+      $(".after-input").animate({ opacity: 1 });
     }, function() {
-      $(".after-input").fadeOut();
+      $(".after-input").animate({ opacity: 0 });
   });
+
+  const $homepage_text = $(".homepage-text p");
+  const $homepage_header = $(".homepage-text h1");
+  const $homepage_blockquote = $(".homepage-text .blockquote");
+
+  //scroll text translate
+  $(window).scroll(function(){
+
+    //scrolltop breakpoints
+    if ($(window).scrollTop() >= 1700){
+        translate = 180;
+    } else if ($(window).scrollTop() >= 1600){
+        translate = 165;
+    } else if ($(window).scrollTop() >= 1400){
+        translate = 150;
+    } else if ($(window).scrollTop() >= 1300){
+        translate = 135;
+    } else if ($(window).scrollTop() >= 1200){
+        translate = 120;
+    } else if ($(window).scrollTop() >= 1100){
+        translate = 105;
+    } else if ($(window).scrollTop() >= 900){
+        translate = 90;
+    } else if ($(window).scrollTop() >= 800){
+        translate = 75;
+    } else if ($(window).scrollTop() >= 700){
+        translate = 60;
+    } else if ($(window).scrollTop() >= 600){
+        translate = 45;
+    } else if ($(window).scrollTop() >= 500){
+        translate = 30;
+    } else if ($(window).scrollTop() >= 400){
+        translate = 15;
+    } else {
+        translate = 0;
+    }
+
+    // if ($(window).scrollTop() <= 300) {
+    //   translate = 0;
+    // } else {
+    //   translate = Math.floor($(window).scrollTop() - 300).toString();
+    //   translate = translate.replace(/^0?$/, '');
+    //   translate = parseInt(translate);
+    // };
+
+    console.log(translate);
+    $homepage_text.css({transform: "translateY(" + translate + "px)"});
+    $homepage_header.css({transform: "translateY(" + translate + "px)"});
+    $homepage_blockquote.css({transform: "translateY(" + translate + "px)"});
+  });
+
 });
