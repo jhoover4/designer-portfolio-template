1b5f19f1e6fd14971f832834344280b008922a85
jhoover4 <jordan@hoovermld.com>
2023-04-02 20:40:39 -0500

    Frozen flask updates.

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ To build the static template, run `python app.py build`.
 Design by
 [Morrison Design Co.](http://morrisondesign.co)
 
-View site on [Heroku](https://jh-designer-portfolio-template.herokuapp.com)
+View site on [Github Pages](https://jhoover4.github.io/designer-portfolio-template)
--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import sys
 
 from flask import Flask, render_template
-from flask_frozen import Freezer  # Added
+from flask_frozen import Freezer
 
 DEBUG = False
 
 app = Flask(__name__)
+app.config['FREEZER_RELATIVE_URLS'] = True
 freezer = Freezer(app)
 
 
@@ -14,9 +15,14 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/work')
+@app.route('/portfolio')
 def portfolio():
     return render_template('portfolio.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == "__main__":
